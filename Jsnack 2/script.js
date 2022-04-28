a470d55b96c471b26d9818ee408d8fe05dd3bf50
SebastianoMV <sebastianovisco@gmail.com>
2022-04-28 17:16:45 +0200

    Jsnack 2 base

--- Jsnack 2/script.js
+++ Jsnack 2/script.js
@@ -1,66 +1,79 @@
-const zucchina0 = {
-  varieta : 'nera',
-  peso : '150',
-  lunghezza : '14',
-};
+const zucchine = [
+  {
+   varieta : 'nera',
+   peso : '150',
+   lunghezza : '15',
+  },
+  {
+    varieta : 'romanesca',
+    peso : '100',
+    lunghezza : '16',
+   },
+   {
+    varieta : 'fiorentina',
+    peso : '200',
+    lunghezza : '20',
+   },
+   {
+    varieta : 'napoletana',
+    peso : '220',
+    lunghezza : '12',
+   },
+   {
+    varieta : 'tonde',
+    peso : '105',
+    lunghezza : '8',
+   },
+   {
+    varieta : 'trombetta',
+    peso : '150',
+    lunghezza : '15',
+   },
+   {
+    varieta : 'gialla',
+    peso : '200',
+    lunghezza : '20',
+   },
+   {
+    varieta : 'friulano',
+    peso : '80',
+    lunghezza : '17',
+   },
+   {
+    varieta : 'crookneck',
+    peso : '120',
+    lunghezza : '18',
+   },
+   {
+    varieta : 'bomacheneso',
+    peso : '90',
+    lunghezza : '22',
+   }
+];
 
-const zucchina1 = {
-  varieta : 'romanesca',
-  peso : '100',
-  lunghezza : '16',
-};
+const zucchineCorte = [];
+const zucchineLunghe = [];
 
-const zucchina2 =   {
-  varieta : 'fiorentina',
-  peso : '200',
-  lunghezza : '20',
-};
 
-const zucchina3 ={
-  varieta : 'napoletana',
-  peso : '220',
-  lunghezza : '12',
-};
-
-const zucchina4 ={
-  varieta : 'tonde',
-  peso : '105',
-  lunghezza : '8',
-};
-
-const zucchina5 = {
-  varieta : 'trombetta',
-  peso : '150',
-  lunghezza : '15',
-};
-
-const zucchina6 = {
-  varieta : 'gialla',
-  peso : '200',
-  lunghezza : '20',
-};
-
-const zucchina7 = {
-  varieta : 'friulano',
-  peso : '80',
-  lunghezza : '17',
-};
-
-const zucchina8 = {
-  varieta : 'crookneck',
-  peso : '120',
-  lunghezza : '18',
-};
-
-const zucchina9 = {
-  varieta : 'bomacheneso',
-  peso : '90',
-  lunghezza : '22',
-};
-
-const zucchineGrandi = [];
-const zucchinePiccole = [];
-
-for(let i = 0; i < 10; i++){
-  if
+for(let index in zucchine){
+  if (zucchine[index].lunghezza > 15) {
+    zucchineLunghe.push(zucchine[index])
+  }else{
+    zucchineCorte.push(zucchine[index])
+  }
 }
+
+let pesoZucchineCorte = 0;
+let pesoZucchineLunghe = 0;
+
+for (let i=0; i<zucchineCorte.length; i++){
+  
+  pesoZucchineCorte += parseInt(zucchineCorte[i].peso);
+};
+
+for (let i=0; i<zucchineLunghe.length; i++){
+  
+  pesoZucchineLunghe += parseInt(zucchineLunghe[i].peso);
+}
+
+document.querySelector('h1').innerHTML = `Le zucchine corte pesano ${pesoZucchineCorte} grammi mentre quelle lunghe pesano ${pesoZucchineLunghe} grammi`;
